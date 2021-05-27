a348cf5d6985e6594bc1ae4f3d36a8edef6b397a
mezelaw <mezequielabogado@gmail.com>
2021-05-26 22:17:38 -0300

    Se agregan tests del service.

--- src/test/java/com/mz/ClimaAPI/ClimaServiceTests.java
+++ src/test/java/com/mz/ClimaAPI/ClimaServiceTests.java
@@ -1,2 +1,43 @@
-package com.mz.ClimaAPI;public class ClimaServiceTests {
+package com.mz.ClimaAPI;
+
+import com.mz.ClimaAPI.dto.ClimaResponseDto;
+import com.mz.ClimaAPI.models.Clima;
+import com.mz.ClimaAPI.services.ClimaServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ClimaServiceTests {
+
+
+    @Autowired
+    ClimaServiceImpl climaService;
+
+
+    @Test
+    public void obtenerClimaDeDiezAnosRetorna3650RegistrosYNoEsNulo(){
+
+        ArrayList<Clima> diezAnosResults = this.climaService.obtenerClimaDeDiezAnos();
+
+        diezAnosResults.stream().map(Clima::getDia)
+                .filter( c -> c != -1).count();
+
+        assertNotNull(diezAnosResults);
+        assertEquals(3650, diezAnosResults.stream().map(Clima::getDia)
+                .filter( c -> c != null).count());
+
+    }
+
+    @Test
+    public void elPrimerDiaEsSequia(){
+        ClimaResponseDto climaResponseDto = this.climaService.calcularClimaPorDia(0);
+        assertEquals(climaResponseDto.getClima(), "Sequia");
+    }
+
 }
